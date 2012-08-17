b1554c2e42c913816fa7c644010bebbae77f49dd
Emil Breding <emil.breding@gmail.com>
2012-08-18 00:24:16 +0200

    Changed to JBoss Netty for network communications, 200% increase in performance!

--- client/src/main/java/se/cygni/texasholdem/client/message/ServerMessageReceiver.java
+++
@@ -1,9 +0,0 @@
-package se.cygni.texasholdem.client.message;
-
-import se.cygni.texasholdem.communication.message.TexasMessage;
-
-public interface ServerMessageReceiver {
-
-	void onMessageReceived(TexasMessage message);
-
-}
---
+++ common/src/main/java/se/cygni/texasholdem/communication/netty/JsonDelimiter.java
@@ -0,0 +1,4 @@
+package se.cygni.texasholdem.communication.netty;
+
+public class JsonDelimiter {
+}
