4862a39217cee54c4d3a5f8b09b71cc0955f57f6
emilb <emil.breding@gmail.com>
2012-10-04 20:00:27 +0200

    Added control for message version on communication between client and server

--- common/src/main/java/se/cygni/texasholdem/communication/message/TexasMessage.java
+++ common/src/main/java/se/cygni/texasholdem/communication/message/TexasMessage.java
@@ -5,9 +5,19 @@ public abstract class TexasMessage {
 
     private final String type = this.getClass().getCanonicalName();
 
+    // The version of the client or server that this message originated from
+    private String version;
+
     public String getType() {
 
         return type;
     }
 
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
 }
