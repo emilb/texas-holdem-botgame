ef364f1aa058047fb28bd1fde47a621ea088ded6
Emil Breding <emil.breding@gmail.com>
2013-03-06 23:20:09 +0100

    Started on clean frontend in angular js

---
+++ frontend/src/main/webapp/app/pokerfront.js
@@ -0,0 +1,10 @@
+/*global angular*/
+/*jshint unused:false*/
+'use strict';
+
+/**
+ * The main pokerfront app module.
+ *
+ * @type {angular.Module}
+ */
+var pokerfrontmvc = angular.module('pokerfrontmvc', []);
